9d995dff9ff79745003ab6d66737181cb16846c3
leeyazhou <bytesgo@163.com>
2020-07-31 10:02:26 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,104 +1,72 @@
 module.exports = {
-  title: "bytesgo.com",
-  description: 'bytesgo.com',
-  dest: 'dist',
-  head: [
-    ['link', { rel: 'icon', href: '/img/favicon.ico' }],
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-    ["script", { src: "/js/demo.js" }]
-  ],
-  theme: 'reco',
-  plugins: ['@vuepress-reco/vuepress-plugin-screenfull'],
-  themeConfig: {
-    smoothScroll:true,
-    nav: [
-      { text: '首页', link: '/', icon: 'reco-home' },
-      { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
-      {
-        text: '文档',
-        icon: 'reco-message',
-        items: [
-          { text: 'api', link: '/api/' }
-        ]
-      },
-      {
-        text: '联系我',
-        icon: 'reco-message',
-        items: [
-          { text: 'GitHub', link: 'https://github.com/bytesgo', icon: 'reco-github' }
-        ]
-      }
+    title: 'bytesgo',
+    description: 'Just playing around',
+    theme: '@vuepress/blog',
+    dest: 'dist',
+    head: [
+        ['link', { rel: 'icon', href: '/img/favicon.ico' }],
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+        ["script", { src: "/js/demo.js" }]
     ],
-    sidebar: {
-      '/api/': [
-        '',
-        'theme',
-        'plugin',
-        'api'
-      ]
+    themeConfig: {
+        dateFormat: 'YYYY-MM-DD',
+        smoothScroll: true,
+        summary: true,
+        lastUpdated: 'Last Updated',
+        nextLinks: true,
+        prevLinks: true,
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '博客', link: '/blog/' },
+            { text: '标签', link: '/tag/' },
+            { text: 'Github', link: 'https://github.com/leeyazhou' }
+        ],
+        directories: [
+            {
+                id: 'post',
+                dirname: 'blog',
+                path: '/blog/',
+                itemPermalink: '/:year/:month/:day/:slug',
+            },
+            {
+                id: 'writing',
+                dirname: '_writings',
+                path: '/',
+                itemPermalink: '/:year/:month/:day/:slug',
+            },
+        ],
+        globalPagination: {
+            lengthPerPage: 15,
+            prevText: '<',
+            nextText: '>',
+        },
+        footer: {
+            contact: [
+                {
+                    type: 'github',
+                    link: 'https://github.com/vuejs/vuepress',
+                },
+                {
+                    type: 'twitter',
+                    link: 'https://github.com/vuejs/vuepress',
+                },
+            ],
+            copyright: [
+                {
+                    text: 'Privacy Policy',
+                    link: 'https://policies.google.com/privacy?hl=en-US',
+                },
+                {
+                    text: 'MIT Licensed | Copyright © 2020-2021 bytesgo.com',
+                    link: 'github.com/bytesgo',
+                },
+            ],
+        },
     },
-    type: 'blog',
-    // 博客设置
-    blogConfig: {
-      category: {
-        location: 2, // 在导航栏菜单中所占的位置，默认2
-        text: '分类' // 默认 “分类”
-      },
-      tag: {
-        location: 3, // 在导航栏菜单中所占的位置，默认3
-        text: '标签' // 默认 “标签”
-      }
-    },
-    // friendLink: [
-    //   {
-    //     title: 'leeyazhou',
-    //     desc: 'Enjoy when you can, and endure when you must.',
-    //     email: 'coderhook@gmail.com',
-    //     link: 'https://blog.bytesgo.com'
-    //   },
-    //   {
-    //     title: 'bytesgo.com',
-    //     desc: 'A simple and beautiful vuepress Blog & Doc theme.',
-    //     link: 'https://blog.bytesgo.com'
-    //   },
-    // ],
-    logo: '/img/logo.png',
-    // 搜索设置
-    search: true,
-    searchMaxSuggestions: 10,
-    // 自动形成侧边导航
-    // sidebar: 'auto',
-    // 最后更新时间
-    lastUpdated: 'Last Updated',
-    // 作者
-    author: 'leeyazhou',
-    // 作者头像
-    // authorAvatar: '/avatar.png',
-    // 备案号
-    // record: 'xxxx',
-    // 项目开始时间
-    startYear: '2020',
-    showAccessNumber:true
-    /**
-     * 密钥 (if your blog is private)
-     */
-
-    // keyPage: {
-    //   keys: ['your password'],
-    //   color: '#42b983',
-    //   lineColor: '#42b983'
-    // },
-
-    /**
-     * valine 设置 (if you need valine comment )
-     */
-
-    // valineConfig: {
-    //   appId: '...',// your appId
-    //   appKey: '...', // your appKey
-    // }
-  },
-  markdown: {
-    lineNumbers: true
-  }
-}  
+    plugins: [
+        '@vuepress/nprogress',
+        '@vuepress/back-to-top',
+        '@vuepress/last-updated',
+        '@vuepress/medium-zoom'
+    ]
+}
--- docs/README.md
+++ docs/README.md
@@ -1,15 +1,12 @@
 ---
-home: true
-heroText: bytesgo.com
-tagline: do share something
-# heroImage: /img/hero.png
-# bgImage: /img/method-draw-image.svg
-heroImageStyle: {
-  maxWidth: '600px',
-  width: '100%',
-  display: block,
-  margin: '9rem auto 2rem',
-  background: '#fff',
-  borderRadius: '1rem',
-}
+title: about me
+author: leeyazhou
 ---
+
+## Hi, nerds. I'm leeyazhou
+
+I code on occasion, believe it or not. My latest deal is I'm making as many useful things with Java and Github as humanly possible. I also have some non-coding things that I update on occasion, for example my blog repo.
+
+And I have a bady son named Zexi Li, so I have to work hard now.
+
+Wish you have a nice day.
