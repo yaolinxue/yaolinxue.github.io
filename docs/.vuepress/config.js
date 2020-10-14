26bb2882e9d9486289cc90016c8a38bc4624b389
leeyazhou <bytesgo@163.com>
2020-10-14 13:06:59 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,10 +4,20 @@ module.exports = {
     theme: '@vuepress/blog',
     dest: 'dist',
     head: [
-        ['link', { rel: 'icon', href: '/img/favicon.ico' }],
-        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
-        ["script", { src: "/js/demo.js" }],
-        ["script", { src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" }]
+        ['link', {
+            rel: 'icon',
+            href: '/img/favicon.ico'
+        }],
+        ['meta', {
+            name: 'viewport',
+            content: 'width=device-width,initial-scale=1,user-scalable=no'
+        }],
+        ["script", {
+            src: "/js/demo.js"
+        }],
+        ["script", {
+            src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        }]
     ],
     themeConfig: {
         dateFormat: 'YYYY年MM月',
@@ -16,14 +26,24 @@ module.exports = {
         lastUpdated: 'Last Updated',
         nextLinks: true,
         prevLinks: true,
-        nav: [
-            { text: '首页', link: '/' },
-            { text: '博客', link: '/blog/' },
-            { text: '标签', link: '/tag/' },
-            { text: 'Github', link: 'https://github.com/leeyazhou' }
-        ],
-        directories: [
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
             {
+                text: '博客',
+                link: '/blog/'
+            },
+            {
+                text: '标签',
+                link: '/tag/'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/bytesgo'
+            }
+        ],
+        directories: [{
                 id: '博客分享',
                 dirname: 'blog',
                 path: '/blog/',
@@ -36,24 +56,40 @@ module.exports = {
                 itemPermalink: '/:year/:month/:day/:slug',
             },
         ],
+        comment: {
+            // Which service you'd like to use
+            service: 'vssue',
+            // The owner's name of repository to store the issues and comments.
+            owner: 'leeyazhou',
+            // The name of repository to store the issues and comments.
+            repo: 'bytesgo.com',
+            // The clientId & clientSecret introduced in OAuth2 spec.
+            clientId: 'f524e60f55892f87f3cb',
+            clientSecret: '306bffa447309506bc034a4529aac0408695114a',
+        },
+        sitemap: {
+            hostname: 'https://www.bytesgo.com'
+        },
+        newsletter: {
+            // Put your endpoint, not mine.
+            endpoint: "https://bytesgo.us2.list-manage.com/subscribe/post?u=648a673e513d2f974345a2532&amp;id=3be239bd6a"
+        },
         globalPagination: {
             lengthPerPage: 15,
             prevText: '<',
             nextText: '>',
         },
         footer: {
-            contact: [
-                {
+            contact: [{
                     type: 'github',
                     link: 'https://github.com/bytesgo',
                 },
                 {
                     type: 'twitter',
-                    link: 'https://twitter.com/leeyazhou',
+                    link: 'https://twitter.com/bytesgo',
                 },
             ],
-            copyright: [
-                {
+            copyright: [{
                     text: 'Privacy Policy',
                     link: 'https://policies.google.com/privacy?hl=en-US',
                 },
@@ -67,9 +103,13 @@ module.exports = {
     markdown: {
         lineNumbers: true,
         // markdown-it-anchor 的选项
-        anchor: { permalink: true },
+        anchor: {
+            permalink: true
+        },
         // markdown-it-toc 的选项
-        toc: { includeLevel: [1, 2] }
+        toc: {
+            includeLevel: [1, 2]
+        }
     },
     plugins: [
         '@vuepress/nprogress',
